d9ff624c3166bf4a112d91e82945ad00c0ed664f
synku20777 <nestor.kulik@gmail.com>
2024-12-15 23:20:58 +0100

    tried to work the router

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,16 @@ import Header from '@/components/header.vue'
 import { useQuery } from '@vue/apollo-composable'
 import { GET_AUTHENTICATED_USER } from '@/graphql/queries/user.query'
 
-const authUser = true
+// const authUser = true
 const { loading, result: data, error } = useQuery(GET_AUTHENTICATED_USER)
 console.log('Loading:', loading)
 console.log('User:', data.value)
 console.log('Error:', error)
-
-// const isAuthenticated = computed(() => {
-//   return data?.value && data?.value.authenticatedUser
-// })
 </script>
 
 <template>
   <div class="flex md:flex flex-col">
-    <div class="border-b">
+    <div v-if="data.authUser" class="border-b">
       <div class="flex h-16 items-center px-4">
         <Header />
       </div>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useQuery } from '@vue/apollo-composable'
 import HomeView from '../views/HomeView.vue'
 import login from '../views/loginView.vue'
 import signup from '../views/signupView.vue'
 import Settings from '@/views/Settings.vue'
-// import { GET_AUTHENTICATED_USER } from '@/graphql/queries/user.query'
-// import { useQuery } from '@vue/apollo-composable'
+import { GET_AUTHENTICATED_USER } from '@/graphql/queries/user.query'
+import { ref, watch } from 'vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -40,14 +41,20 @@ const router = createRouter({
   ],
 })
 
-// const { loading, error, data } = useQuery(GET_AUTHENTICATED_USER)
+const { loading, result: data } = useQuery(GET_AUTHENTICATED_USER)
 
-// console.log('loading', loading)
-// console.log('auth user:', data)
-// console.log('error:', error)
+router.beforeEach((to, from, next) => {
+  if (loading.value) return next(false)
 
-// if (loading) {
-//   console.log('Loading...')
-// }
+  const authUser = data.value?.authUser
+
+  if (to.meta.requiresAuth && !authUser) {
+    next({ path: '/login' })
+  } else if (to.meta.guestOnly && authUser) {
+    next({ path: '/' })
+  } else {
+    next()
+  }
+})
 
 export default router
